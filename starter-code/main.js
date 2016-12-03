b8f2f2d31136a030b27572267812bc16f4445bd6
Esther Low <phi21416@gmail.com>
2016-12-03 23:26:18 +0800

    compare cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king"
+
+if (cardTwo === cardFour){
+	alert('You found a match');
+} else if (cardTwo === cardThree){
+	alert('You found a match');
+} else if (cardTwo === CardOne){
+	alert('You found a match');
+} else if (cardThree ==== cardOne){
+	alert('You found a match');
+} else if (cardThree === cardFour){
+	alert('You found a match');
+} else if (cardFour === cardOne){
+	alert('You found a match');
+} else {
+
+	alert('Sorry, try again');
+}
