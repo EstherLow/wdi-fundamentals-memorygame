7ae7767406dc3bbdf9cd21bad98f6400e452821d
Esther Low <phi21416@gmail.com>
2016-12-05 06:19:32 +0800

    added all script

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,59 @@
-console.log("JS file is connected to HTML! Woo!")
+/*console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king"
+*/
 
+var gameBoard = document.getElementById("game-board");
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+function createBoard(){
+for (var i = 0; i < cards.length; i++) {
+	var cardElment = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+	
+	gameBoard.appendChild(cardElement);
+	gameBoard.appendChild(cardElement);
+
+}
+}
+
+function isMatch(cards){
+if (cards[0] === cards[1]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, tryagain.");
+}
+
+}
+
+function isTwoCards() {
+cardsInPlay.push(this.getAttribute('data-card'));
+if (this.getAttribute('data-card') === 'king'){
+	this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2015/08/11/11/57/spades-884197_960_720.png'>";
+} else {
+	this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2015/08/11/11/57/spades-884203_960_720.png'>";
+}
+if (cardsInPlay.length === 2){
+	isMatch(cardsInPlay);
+
+	cardsInPlay = [];
+}
+
+}
+
+
+
+
+createBoard();
+
+/*
 if (cardTwo === cardFour){
 	alert('You found a match');
 } else if (cardTwo === cardThree){
@@ -20,3 +70,5 @@ if (cardTwo === cardFour){
 
 	alert('Sorry, try again');
 }
+
+*/
